613479ade782bcb633cb629ceea30bb98e3dfdab
Thienbinh2504 <99796227+Thienbinh2504@users.noreply.github.com>
2022-02-16 16:06:55 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Làm người yêu tớ nhé I Love You",
+  titleHeader: "Hứa với anh ngày nào cũng uống nước nhiều vô nhé !!!",
   descriptionHeader:
-    "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
+    "Nếu em thoát ra và không trả lời thì có nghĩa là em đã ĐỒNG Ý rồi đấy ^^",
   buttonYes: "Đồng Ý",
-  buttonNo: "Không! Còn lâu nhé.",
-  titleModar: "Cảm ơn em vì đồng ý",
+  buttonNo: "Mơ đi cưng. Đá 1 cái bây giờ .",
+  titleModar: "Cảm ơn em vì đã  đồng ý",
   descriptionModar:
-    'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+    'Anh biết là em sẽ đồng ý nghe lời anh mà ^^ </br> Cám ơn em đã đồng ý để cho anh được che chở và bảo vệ em. Đối với anh, em mãi là cô bé :)) </br> "Thích em nhiều đến mấy, em sẽ hiểu thôi. </br>  Anh yêu em <3 "',
 };
